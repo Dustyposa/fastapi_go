b64a55e86988763eeb2354647dd4da277266a863
zmzposa <zmzposa@outlook.com>
2020-01-16 18:12:48 +0800

    :memo: set cookie

---
+++ src/26_Response_Cookies.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from starlette.responses import Response, JSONResponse
+
+app = FastAPI()
+
+
+@app.post("/cookie-and-object/")
+def create_cookie(response: Response):
+    response.set_cookie(key="fakesession", value="fake-cookie-session-value")
+    return {"message": "Come to the dark side, we have cookies"}
+
+
+@app.post("/cook/")
+def create_cook():
+    content = {"message": "Come to the dark side, we have cookies"}
+    response = JSONResponse(content=content)
+    response.set_cookie(key="fakesession", value="fake-cookie-session-value")
+    return response
