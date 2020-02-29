9b059a57e3b3b1b0fe34723924701ac41ccb49c8
zmzposa <zmzposa@outlook.com>
2020-02-29 11:37:25 +0800

    :memo: security

--- src/dependencies/6_advance_dependencies.py
+++ src/dependencies/6_advance_dependencies.py
@@ -19,5 +19,3 @@ checker = FixedContentQueryChecker("bar")
 @app.get("/query-checker/")
 async def read_query_check(fixed_content_included: bool = Depends(checker)):
     return {"fixed_content_in_query": fixed_content_included}
-
-
